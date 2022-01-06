9f42db8d641f302ad3d080335b0e607861c42103
kyousei-dev <matej.medved01@gmail.com>
2022-01-06 20:39:47 +0100

    drawing

--- app.js
+++ app.js
@@ -65,6 +65,16 @@ function drawMatrix(matrix){
     }
 }
 
+function drawDAWAY(path,size){
+
+    let j = (path % (size-2))+1
+    let i = Math.floor(path / (size-2))+1
+    ctx.beginPath();
+    ctx.rect(j* (canvas.width/size), i* (canvas.height/size),(canvas.width/size),(canvas.width/size));
+    ctx.fill();
+    
+}
+
 function matrixToGraph(matrix){
     //console.log(matrix.length, matrix[0].length)
     let graph = Array((matrix.length-2) * (matrix[0].length-2)).fill();
@@ -159,25 +169,30 @@ function BFS(graph, startNode, endNodes){
     marked[startNode] = true;
 
     queue.push(startNode);
-    console.log("Dajem v vrsto vozlisce " + startNode);
+    //console.log("Dajem v vrsto vozlisce " + startNode);
 
     while(queue.length>0){
         let curNode = queue.shift();
-        console.log("Odstranjujem iz vrste vozlisce " + curNode);
+        //console.log("Odstranjujem iz vrste vozlisce " + curNode);
 
-        if(endNodes.incudes(curNode)){
-            console.log("Resitev BFS v vozliscu " + curNode);
+        if(endNodes.includes(curNode)){
+            //console.log("Resitev BFS v vozliscu " + curNode);
             console.log("Pot: " + curNode);
+            let finish = curNode;
+            let path = [curNode];
             while(true){
                 curNode = from[curNode];
                 if(curNode != -1){
                     console.log(" <-- " + curNode);
+                    path.push(curNode);
                 }
                 else{
-                    break;
+                    let index = endNodes.indexOf(finish);
+                    endNodes.splice(index,1)
+                    return [finish, endNodes,path];
                 }
             }
-            return;
+            
         }
 
         for(let nextNode = 0; nextNode < graph[curNode].length; nextNode++){
@@ -185,10 +200,31 @@ function BFS(graph, startNode, endNodes){
                 marked[nextNode] = true;
                 from[nextNode] = curNode;
                 queue.push(nextNode);
-                console.log("Dajem v vrsto vozlisce "+ nextNode);
+                //console.log("Dajem v vrsto vozlisce "+ nextNode);
             }
         }
     }
+}
+
+function main(timeStamp){
+
+    if(timeStamp - previousTimeStamp > 500/(matrix.length/2)){
+        previousTimeStamp = timeStamp;
+        if(pot[0].length == 0){
+            if(pot.length != 0){
+                pot.shift();
+                barve.unshift(barve.pop());
+                ctx.fillStyle = barve[0];
+            }
+        }
+        if(pot.length == 0){
+            return;
+        }
+        drawDAWAY(pot[0].pop(),matrix.length);
+    }
+
+
+    window.requestAnimationFrame(main);
 }
 
 let canvas = document.getElementById("Canvas");
@@ -196,11 +232,12 @@ canvas.width = 800;
 canvas.height = 800;
 let ctx = canvas.getContext("2d");
 
-let text = readTextFile("./labyrinths/labyrinth_1.txt");
+let text = readTextFile("./labyrinths/labyrinth_9.txt");
 let matrix = stringToMatrix(text);
 //let visit_matrix = Array(matrix.length).fill().map(() => Array(matrix[0].length).fill(0));
 //consolePrintMatrix(visit_matrix);
 drawMatrix(matrix);
+ctx.fillStyle = "blue"
 
 
 let graph = matrixToGraph(matrix);
@@ -208,9 +245,26 @@ let startNode = graph[2];
 let endNode = graph[3];
 let nodes = graph[1];
 graph = graph[0];
+let vrnjeno = [startNode, JSON.parse(JSON.stringify(nodes))]
 
+console.log("Dolžina nodov",nodes.length)
 
+let pot = []
+let previousTimeStamp = 0;
+let barve = ["pink","red","orange","dodgerblue","darkgreen"]
 
+for(let i = 0 ; i < nodes.length+1; i++){
+    console.log("Dolžina nodov",nodes.length)
+    console.log("Ponovitev številka: ",i)
+    vrnjeno = BFS(graph,vrnjeno[0],vrnjeno[1])
+    pot.push(vrnjeno[2]);
+    if(vrnjeno[1].length == 0){
+        vrnjeno[1].push(endNode);
+    }
+
+}
+
+main();
 
 
 
